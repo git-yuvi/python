5afb4edd72cbf539492b7c888b0ccaee74d95e99
Yuvaraj A <104821565+git-yuvi@users.noreply.github.com>
2022-09-21 23:37:48 -0700

    Create arithmatic.py

---
+++ arithmatic.py
@@ -0,0 +1,25 @@
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
+sum = float(num1) + float(num2)
+print('the sum of {0} and {1} is {2}'.format(num1,num2,sum))
+
+min = float(num1) - float(num2)
+print('the min of {0} and {1} is {2}'.format(num1,num2,min))
+
+mtpy = float(num1)*float(num2)
+print('the multiple of {0} and {1} is {2}'.format(num1,num2,mtpy))
+
+div = float(num1)/float(num2)
+print('the divition of {0} and {1} is {2}'.format(num1,num2,div))
+
+floor = float(num1)//float(num2)
+print('the floor of {0} and {1} is {2}'.format(num1,num2,floor))
+
+
+mod = float(num1)%float(num2)
+print('the mod of {0} and {1} is {2}'.format(num1,num2,mod))
+
+power = float(num1)**float(num2)
+print('the power of {0} and {1} is {2}'.format(num1,num2,power))
+
+input()
